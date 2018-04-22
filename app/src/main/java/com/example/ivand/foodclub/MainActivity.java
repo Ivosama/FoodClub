e3115b85e16428b13b90a2a01d642c623528c71f
Ivosama <ivandts@gmail.com>
2018-04-22 14:54:47 +0200

    Third commit creation of two more activities and the buttons for it. Tested.

--- app/src/main/java/com/example/ivand/foodclub/MainActivity.java
+++ app/src/main/java/com/example/ivand/foodclub/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.ivand.foodclub;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button_eat;
+    private Button button_host;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button_eat = (Button) findViewById(R.id.Eat_button);
+        button_host = (Button) findViewById(R.id.Host_button);
+
+        button_eat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMapAndList();
+            }
+        });
+
+        button_host.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHost();
+            }
+        });
+
     }
+
+    public void openMapAndList(){
+        Intent intent = new Intent(this, Map_and_List.class);
+        startActivity(intent);
+    }
+
+    public void openHost(){
+        Intent intent = new Intent(this, Host.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/ivand/foodclub/Map_and_List.java
@@ -0,0 +1,13 @@
+package com.example.ivand.foodclub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Map_and_List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_and__list);
+    }
+}
