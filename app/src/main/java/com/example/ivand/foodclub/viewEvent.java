57ccd5d338e8e1de58b858dcefb46deffbf09dd5
PeterBusk <pknuds15@student.aau.dk>
2018-05-01 11:54:42 +0200

    Clickable buttons in Map_and_list takes you to viewEvent

--- app/src/main/java/com/example/ivand/foodclub/viewEvent.java
+++ app/src/main/java/com/example/ivand/foodclub/viewEvent.java
@@ -20,10 +20,9 @@ public class viewEvent extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Messages not yet implemented", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
     }
-
 }
