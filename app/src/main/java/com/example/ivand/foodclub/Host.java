d6943df7d4c772b816736f6daf69d055caae6b90
PolleAnker <thementoryay@gmail.com>
2018-05-03 13:33:59 +0200

    Time picker dialog exists, time input is not saved or used yet though

--- app/src/main/java/com/example/ivand/foodclub/Host.java
+++ app/src/main/java/com/example/ivand/foodclub/Host.java
@@ -1,14 +1,27 @@
 package com.example.ivand.foodclub;
 
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.VectorEnabledTintResources;
 import android.text.TextUtils;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
+
+import static java.lang.String.valueOf;
 
 public class Host extends AppCompatActivity {
 
@@ -55,6 +68,7 @@ public class Host extends AppCompatActivity {
                         price = 0;
                     }
 
+
                     Event event = new Event(ID, dist, eventName, whatCooking, place, description, price);
 
                     confirmPost(event);
@@ -84,8 +98,36 @@ public class Host extends AppCompatActivity {
             confirm.create().show();
     }
 
+    // Class and functionality for the time picker class, used to make a dialog
+    //for picking time when hosting an event.
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+        }
+
+    }
+
+    // Opens time picker dialog for setting time of event
+    public void showTimePickerDialog(View v) {
+            DialogFragment newFragment = new TimePickerFragment();
+            newFragment.show(getFragmentManager(), "timePicker");
+
+    }
 
 }
-
 
 
