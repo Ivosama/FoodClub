3d8dbfd532b59e55e880d98e124740e07a2d8966
PolleAnker <thementoryay@gmail.com>
2018-05-04 10:53:31 +0200

    12,5 is 12:30 now

--- app/src/main/java/com/example/ivand/foodclub/MainActivity.java
+++ app/src/main/java/com/example/ivand/foodclub/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
         // ARRAY STUFF
 
-        Event fakeEvent1 = new Event(0, 1, "Poul's Event motherfuckers!", "Pasta ala Poul", "Poul's place, which is very nice and large and good and the windows are oh so fine. The place is located in Nørresundby which is kindof not 10 minutes from basis", "Hey all! Come eat some of my delicious pasta. Oh, and btw - I am wild!","12,5", 5);
+        Event fakeEvent1 = new Event(0, 1, "Poul's Event motherfuckers!", "Pasta ala Poul", "Poul's place, which is very nice and large and good and the windows are oh so fine. The place is located in Nørresundby which is kindof not 10 minutes from basis", "Hey all! Come eat some of my delicious pasta. Oh, and btw - I am wild!","12:30", 5);
         Event fakeEvent2 = new Event(1, 1, "BASISBAR TODAY!", "Beers, en masse!", "BasisBar, of course!", "Fucking Basisbar, what more is there to say?!?!?", "14:00",  10);
 
         Role fakeRole1 = new Role("DishWasher");
